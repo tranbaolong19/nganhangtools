ceb2b515bf18d5d8c517f2174a18eab19edd8012
tranbaolong19 <shopdichvumxh19022008@gmail.com>
2024-11-09 21:51:03 +0700

    Update backgroundImage.ts
    Tôi chắc chắn muốn thay đổi

--- .stackbit/models/backgroundImage.ts
+++ .stackbit/models/backgroundImage.ts
@@ -4,12 +4,22 @@ export const backgroundImage: ModelExtension = {
     name: 'backgroundImage',
     fields: [
         {
+            name: 'imageUrl',
+            controlType: 'text', // Cho phép người dùng nhập URL hình ảnh
+            label: 'Image URL',
+            description: 'Enter the URL of the background image',
+            required: true,
+            value: 'https://drive.google.com/file/d/1LUy1TLSAFc8Rnq5tr6IL5we230Oh_sKC/view?usp=drive_link' // URL hình ảnh nền được điền sẵn
+        },
+        {
             name: 'opacity',
             controlType: 'slider',
             min: 0,
             max: 100,
             step: 1,
-            unit: '%'
+            unit: '%',
+            label: 'Opacity',
+            description: 'Adjust the opacity of the background image'
         }
     ]
 };
